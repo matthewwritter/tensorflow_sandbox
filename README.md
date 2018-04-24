2a1aa19834aa2f432ac40aadb8f573f0db991cf2
Matt Ritter <matthew.w.ritter@gmail.com>
2018-04-24 08:02:59 -0400

    First working code using TensorFlow

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # tensorflow_sandbox
 ## TODO
+* Play around with basic TF functions
+	* Definition of Done: Have touched 80% of the functions on the documentation page
 * Implement linear regression
 	* Definition of Done: 100M noisy points are fit, random subsample visualized
--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import numpy as np
+import tensorflow as tf
+
+x1 = tf.placeholder(tf.float32)
+x2 = tf.placeholder(tf.float32)
+
+sum_op = tf.add(x1, x2)
+
+with tf.Session() as session:
+    sum_result = session.run(sum_op, feed_dict={x1: 2.0, x2: 0.5})
+
+print(sum_result)
